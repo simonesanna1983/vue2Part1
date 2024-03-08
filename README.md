6daa9d1ccca17950cb135bb33a999c6538c153c9
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2024-03-08 14:36:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2Part1
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/simonesanna1983/vue2Part1)
---
+++ app.js
@@ -0,0 +1,9 @@
+import './style.css';
+// import { setupCounter } from './counter.js'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+  },
+});
